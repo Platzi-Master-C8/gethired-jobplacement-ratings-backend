e7454c2b3d67ae6367dcf09648729384859a0840
Javier Amaya <Javieramayapat@gmail.com>
2022-01-05 11:46:11 -0600

    feat: Add Applicant model

--- ratings/models/models.py
+++ ratings/models/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String, DECIMAL, false, Date
+from sqlalchemy.sql.sqltypes import TIMESTAMP
 from ratings.config.database import Base
 
 
@@ -16,7 +17,7 @@ class CompanyEvaluation(Base):
     job_location = Column(String(70), nullable=false)
     applicant_email = Column(String(70), nullable=false)
     start_date = Column(Date, nullable=True)
-    end_date = Column(Date,nullable=True)
+    end_date = Column(Date, nullable=True)
     is_still_working_here = Column(Integer)
     salary = Column(DECIMAL(14, 2), nullable=false)
     currency_type = Column(String(5), nullable=false)
@@ -27,3 +28,15 @@ class CompanyEvaluation(Base):
     utility_counter = Column(Integer, default=0)
     non_utility_counter = Column(Integer, default=0)
     # created_at = Column(DateTime(), default=datetime.now())
+
+
+class Applicant(Base):
+    __tablename__ = "applicants"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50), nullable=False)
+    email = Column(String(70), nullable=False)
+    address = Column(String(70), nullable=True)
+    telephone = Column(Integer(10), nullable=True)
+    linkedln_url = Column(String(150), nullable=False)
+    cv_url = Column(String(150), nullable=False)
+    motivation_letter_url = Column(String(150), nullable=True)
